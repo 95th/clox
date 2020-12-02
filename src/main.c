371a90e947e6534cb42c8a86dbb6d874fa494e2f
Gurwinder Singh <vargwin@gmail.com>
2020-12-02 20:47:17 +0530

    vm

--- src/main.c
+++ src/main.c
@@ -1,17 +1,23 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char** argv) {
+    initVM();
+
     Chunk chunk;
     init_chunk(&chunk);
 
     int constant = add_constant(&chunk, 1.2);
     write_chunk(&chunk, OP_CONSTANT, 123);
     write_chunk(&chunk, constant, 123);
-
     write_chunk(&chunk, OP_RETURN, 123);
+
     disassemble_chunk(&chunk, "test chunk");
+    interpret(&chunk);
+
+    freeVM();
     free_chunk(&chunk);
     return 0;
 }
---
+++ src/vm.c
@@ -0,0 +1,36 @@
+#include "vm.h"
+#include "common.h"
+#include <stdio.h>
+
+VM vm;
+
+void initVM() {}
+void freeVM() {}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;) {
+        uint8_t instr = READ_BYTE();
+        switch (instr) {
+        case OP_CONSTANT: {
+            Value constant = READ_CONSTANT();
+            print_value(constant);
+            printf("\n");
+            break;
+        }
+        case OP_RETURN:
+            return INTERPRET_OK;
+        }
+    }
+
+#undef READ_CONSTANT
+#undef READ_BYTE
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
