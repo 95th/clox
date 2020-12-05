0ce873c583f789a47f45ae9a65fa82afe56435f4
Gurwinder Singh <vargwin@gmail.com>
2020-12-05 11:47:08 +0530

    Begin scanner

--- src/main.c
+++ src/main.c
@@ -1,26 +1,22 @@
 #include "chunk.h"
 #include "common.h"
 #include "vm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, const char** argv) {
     initVM();
 
-    Chunk chunk;
-    init_chunk(&chunk);
-
-    int constant = add_constant(&chunk, 1.2);
-    write_chunk(&chunk, OP_CONSTANT, 123);
-    write_chunk(&chunk, constant, 123);
-    write_chunk(&chunk, OP_NEGATE, 123);
-    int constant2 = add_constant(&chunk, 2.3);
-    write_chunk(&chunk, OP_CONSTANT, 123);
-    write_chunk(&chunk, constant2, 123);
-    write_chunk(&chunk, OP_ADD, 123);
-    write_chunk(&chunk, OP_RETURN, 123);
-
-    interpret(&chunk);
+    if (argc == 1) {
+        repl();
+    } else if (argc == 2) {
+        run_file(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: clox [path]\n");
+        exit(64);
+    }
 
     freeVM();
-    free_chunk(&chunk);
     return 0;
 }
--- src/vm.h
+++ src/vm.h
@@ -3,6 +3,7 @@
 
 #include "chunk.h"
 #include "value.h"
+#include <stdio.h>
 
 #define STACK_MAX 256
 
@@ -22,8 +23,64 @@ typedef enum {
     INTERPRET_RUNTIME_ERROR,
 } InterpretResult;
 
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
+
+static void repl() {
+    char line[2048];
+    for (;;) {
+        printf("> ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            print("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static char* read_file(const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file %s.\n", path);
+        exit(74);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t file_size = ftell(file);
+    rewind(file);
+
+    char* buffer = (char*)malloc(file_size + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read %s.\n", path);
+        exit(74);
+    }
+
+    size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+    if (bytes_read < file_size) {
+        fprintf(stderr, "Could not read file %s.\n", path);
+        exit(74);
+    }
+
+    buffer[bytes_read] = '\0';
+    fclose(file);
+    return buffer;
+}
+
+static void run_file(const char* path) {
+    char* source = read_file(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR) {
+        exit(65);
+    }
+
+    if (result == INTERPRET_RUNTIME_ERROR) {
+        exit(70);
+    }
+}
 
 #endif
